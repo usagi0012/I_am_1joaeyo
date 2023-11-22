44c99e6f5070e2515241917e08f43eafc5938022
jiwon <usagi0012@naver.com>
2023-11-22 16:38:54 +0900

    본인 회원정보 조회 API

--- backend/routers/users/users.router.js
+++ backend/routers/users/users.router.js
@@ -1,5 +1,30 @@
-import { Router } from 'express';
+import express from 'express';
+import db from '../../models/index.cjs';
 
-const usersRouter = Router();
+const usersRouter = express.Router();
+const { Users } = db;
+
+//회원 정보 조회 API
+
+// 미들웨어 만들면 가져오기
+// import authMiddleware from '../../middleware/auth.middleware';
+
+usersRouter.get('/members', async (req, res) => {
+    try {
+        // const userId = res.locals.user;
+        const id = 1;
+        const data = await Users.findOne({
+            attributes: ['nickname', 'description'],
+            where: { id },
+        });
+        res.status(200).json({
+            success: true,
+            message: '회원 상세조회 성공',
+            data,
+        });
+    } catch (error) {
+        console.log(error);
+    }
+});
 
 export default usersRouter;
