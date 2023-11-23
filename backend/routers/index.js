bbed68915619f9faa4b1a383722d234a68e104a5
kim <iufan1526@gmail.com>
2023-11-23 19:31:07 +0900

    덧글 crud 추가

--- backend/routers/index.js
+++ backend/routers/index.js
@@ -1,12 +1,12 @@
 import { Router } from 'express';
-import postsRouter from './posts/posts.router.js';
+import postsRouter from './posts/posts.deatil.router.js';
 import commentsRouter from './posts/comments/comments.router.js';
 import authRouter from './auth/auth.router.js';
 import userRouter from './users/users.router.js';
 
 const apiRouter = Router();
 
-apiRouter.use('/posts', postsRouter);
+apiRouter.use('/posts', [postsRouter, commentsRouter]);
 apiRouter.use('/comments', commentsRouter);
 apiRouter.use('/authRouter', authRouter);
 apiRouter.use('/user', userRouter);
