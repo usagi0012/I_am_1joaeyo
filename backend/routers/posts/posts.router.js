19af0a619fc234155f516c16c31c5be35846c2f0
jiwon <usagi0012@naver.com>
2023-11-23 20:16:04 +0900

    게시글 관리 API

--- backend/routers/posts/posts.router.js
+++ backend/routers/posts/posts.router.js
@@ -5,75 +5,7 @@ import moment from 'moment-timezone';
 import { needSignin } from '../../middleware/auth.middleware.js';
 
 const postsRouter = express.Router();
-const { Posts, Users, Likes } = db;
-
-/**
- * 게시글 조회 API
- */
-postsRouter.get('/', async (req, res) => {
-    const posts = await Posts.findAll({
-        attributes: ['id', 'title', 'content', 'userId', 'createdAt'],
-        include: [
-            {
-                model: Users,
-                as: 'user',
-                attributes: ['nickname'],
-            },
-        ],
-    });
-
-    const resultPosts = posts.map(ele => {
-        return ele.toJSON();
-    });
-
-    resultPosts.forEach(ele => {
-        ele.createdAt = moment(ele.createdAt).format('YYYY-MM-DD hh:mm:ss');
-    });
-
-    res.status(200).json(Response.successResult('게시글 조회 성공', resultPosts));
-});
-
-/**
- * 게시글 상세 조회 API
- */
-postsRouter.get('/:postId', async (req, res) => {
-    const { postId } = req.params;
-
-    const resultPost = await Posts.findOne({
-        where: {
-            id: postId,
-        },
-        include: [
-            {
-                model: Users,
-                as: 'user',
-                attributes: ['nickname'],
-            },
-        ],
-    });
-
-    if (!resultPost) {
-        return res.status(400).json(Response.failResult('해당 게시글이 존재하지 않습니다.'));
-    }
-
-    const successPost = resultPost.toJSON();
-    successPost.createdAt = moment(resultPost.createdAt).format('YYYY-MM-DD hh:mm:ss');
-
-    res.status(200).json(Response.successResult('게시글 상세 조회 성공', successPost));
-});
-
-/**
- * 테스트용 데이터 생성 API
- */
-postsRouter.post('/testCreate', async (req, res) => {
-    const result = await Posts.create({
-        title: '테스트 포스트',
-        content: '테스트 컨텐츠',
-        userId: 1,
-    });
-
-    res.status(200).json(result);
-});
+const { Posts } = db;
 
 // 게시글 작성 API
 postsRouter.post('/', needSignin, async (req, res) => {
@@ -134,7 +66,7 @@ postsRouter.put('/:postId', needSignin, async (req, res) => {
                 ...(content && { content }),
                 ...(image && { image }),
             },
-            { where: { id: postId } }
+            { where: { id: postId } },
         );
 
         //
