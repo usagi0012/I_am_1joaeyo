348b42b1a78009a939faa05d50215caf882dd5de
unknown <rladydrms98@naver.com>
2023-11-25 19:00:19 +0900

    회원가입 기능 연결 미완성
    - 아직미완성

--- frontend/webapp/js/signup.js
+++ frontend/webapp/js/signup.js
@@ -1,35 +1,32 @@
 const submitBtn = document.getElementById('submitBtn');
-const signupBtn = document.getElementById('signupBtn');
-
-submitBtn.addEventListener('click', async function (event) {
-    event.preventDefault();
-
+submitBtn.addEventListener('click', async event => {
+    const nickname = document.querySelector('[name="nickname"]').value;
     const email = document.querySelector('[name="email"]').value;
     const password = document.querySelector('[name="password"]').value;
+    const passwordConfirm = document.querySelector('[name="passwordConfirm"]').value;
     try {
-        const response = await fetch('http://localhost:3050/authRouter/signin', {
+        const response = await fetch('http://localhost:3050/authRouter/signup', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
             },
             body: JSON.stringify({
+                nickname: nickname,
                 email: email,
                 password: password,
+                passwordConfirm: passwordConfirm,
             }),
         });
 
         const data = await response.json();
         console.log(data);
         if (data.success) {
-            window.location.href = 'index.html';
+            alert('회원가입 완료되었습니다!');
+            window.location.href = 'login.html';
         } else {
-            alert('아이디 비밀번호를 확인해주세요.');
+            alert('입력 정보를 확인해주세요.');
         }
     } catch (error) {
         alert('알 수 없는 오류가 발생하였습니다. 관리자에게 문의하세요.');
     }
-});
-
-signupBtn.addEventListener('click', async function (event) {
-    window.location.href = 'signup.html';
 });
