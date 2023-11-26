60e5dcedf6f7db70d5e4e47d12f5408986bb5d79
jiwon <usagi0012@naver.com>
2023-11-26 18:24:30 +0900

    본인 댓글만 수정/삭제 가능하게 변경

--- frontend/webapp/js/detailpost.js
+++ frontend/webapp/js/detailpost.js
@@ -3,11 +3,11 @@ function toHome() {
 }
 
 const content = document.getElementById('content');
+const commentContainer = document.getElementById('commentContainer');
 
 window.onload = loadPost(24);
 
 function loadPost(postId) {
-    console.log('hi');
     fetch(`http://localhost:3050/posts/${postId}`, {
         method: 'GET',
         headers: {
@@ -17,11 +17,12 @@ function loadPost(postId) {
         .then(res => res.json())
         .then(res => {
             loadContent(res.data);
-            console.log(res.data);
         })
+        .then(loadComment(postId))
         .catch(err => alert('데이터 정보를 불러오지 못했습니다. 에러 : ' + err));
 }
 
+//글 내용 불러오기
 function loadContent(e) {
     content.innerHTML = '';
     content.innerHTML += `
@@ -42,4 +43,33 @@ function loadContent(e) {
 <div class="contentCotainer">
 ${e.content}
 </div>`;
+}
+
+//댓글 불러오기
+function loadComment(postId) {
+    fetch(`http://localhost:3050/posts/${postId}/comments`, {
+        method: 'GET',
+        headers: {
+            Accept: 'application/json',
+        },
+    })
+        .then(res => res.json())
+        // .then(res => console.log(res.data))
+        .then(res => {
+            commentContainer.innerHTML = '';
+            res.data.forEach(e => {
+                if (e.userId)
+                    commentContainer.innerHTML += `<div class="comment">
+                <div class="commentNickname">${e.users.nickname}</div>
+                <div class="commentContent">${e.content}</div>
+                <button type="button" class="editIcon">
+                    <i class="fas fa-solid fa-pen-to-square"></i>
+                </button>
+                <button type="button" class="deleteIcon">
+                    <i class="fas fa-solid fa-trash"></i>
+                </button>
+            </div>`;
+            });
+        })
+        .catch(err => alert('데이터 정보를 불러오지 못했습니다. 에러 : ' + err));
 }
